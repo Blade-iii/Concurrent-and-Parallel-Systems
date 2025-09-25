65ce034bf37dd9f2980dc708cf96e06f3e4c0631
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-09-25 16:36:15 +0100

    Expressions with threads

---
+++ Lab2/Expressions and Threads/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main() {
+    // Create thread using lambda expression
+    thread t([] {cout << "Hello" << endl; });
+    // Join thread
+    t.join();
+    
+    return 0;
+}
