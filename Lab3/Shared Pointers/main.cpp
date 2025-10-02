623748707add6ca1949e8a5df07f4ad17aaf3f14
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-10-02 16:05:33 +0100

    Init

--- Lab3/Shared Pointers/main.cpp
+++ Lab3/Shared Pointers/main.cpp
@@ -22,13 +22,22 @@ void incrementMutex(shared_ptr< int>value) {
     }
 }
 
+void incrementLockGuard(shared_ptr< int>value) {
+    for (unsigned int i = 0; i < 1000000; i++) {
+        // Create lock guard
+        lock_guard<mutex> lock(mut);
+        *value = *value + 1;
+        // Lock guard unlocks after the end of the loop
+    }
+}
+
 int main() {
     auto value = make_shared<int>(0);
     auto numberThreads = thread::hardware_concurrency();
     vector<thread> threads;
     for (unsigned int i = 0; i < numberThreads; i++) {
-        // Change to either increment or incrementMutex
-        threads.push_back(thread(incrementMutex, value));
+        // Change to either increment or incrementMutex or incrementLockGuard
+        threads.push_back(thread(incrementLockGuard, value));
     }
 
     for (auto& thread : threads) {
